5a03a566a6a2e2c13e034c3ec10e1c4176cbf661
vrachv <52053491+vrachv@users.noreply.github.com>
2025-01-31 13:52:02 +0000

    Add declensions names patch

---
+++ WCNU/Patch.cs
@@ -0,0 +1,22 @@
+﻿namespace WCNU;
+
+public class Patch
+{
+    public required string Name { get; init; }
+    public required byte[] Pattern { get; init; }
+    public required byte[] Replace { get; init; }
+    public int Offset { get; init; }
+
+    public byte[] GetExpectedResult()
+    {
+        var result = new byte[Pattern.Length];
+        Array.Copy(Pattern, result, Pattern.Length);
+        
+        for (var i = 0; i < Replace.Length; i++)
+        {
+            result[i] = Replace[i];
+        }
+
+        return result;
+    }
+}
--- WCNU/Program.cs
+++ WCNU/Program.cs
@@ -2,7 +2,21 @@
 
 class Program
 {
-    private const string PatchedName = "wow-wcnu.exe";
+    private const string PatchedName = "wow-wcnu";
+
+    private static readonly Patch CharacterNameUnlockPatch = new()
+    {
+        Name = "Character Name Unlock",
+        Pattern = [0x75, 0x37, 0x8B, 0x4D, 0x0C, 0x85, 0xC9, 0x74, 0x1C, 0x0F, 0xB7, 0x01, 0x66, 0x85, 0xC0, 0x74],
+        Replace = [0xEB]
+    };
+
+    private static readonly Patch DeclensionsNameUnlockPatch = new()
+    {
+        Name = "Declensions Name Unlock",
+        Pattern = [0x0F, 0x85, 0xDB, 0x00, 0x00, 0x00, 0x83, 0xC6, 0x01, 0x83, 0xFE, 0x05, 0x7C, 0xCE, 0x33, 0xF6],
+        Replace = [0x90, 0x90, 0x90, 0x90, 0x90, 0x90]
+    };
 
     private static void Main(string[] args)
     {
@@ -20,55 +34,79 @@ class Program
             PressAnyKeyToExit();
             return;
         }
+        
+        var patchesApplied = false;
+        byte[] originalBytes = File.ReadAllBytes(path);
 
-        try
+        patchesApplied |= TryPatch(ref originalBytes, CharacterNameUnlockPatch);
+        patchesApplied |= TryPatch(ref originalBytes, DeclensionsNameUnlockPatch);
+
+        if (patchesApplied)
         {
-            if (IsPatched(path))
-            {
-                Console.WriteLine("File is already patched");
-                PressAnyKeyToExit();
-                return;
-            }
-            
-            var result = FindAndPatch(path);
-            if (!result)
-            {
-                Console.WriteLine("Target pattern not found in the file");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
+            var newPath = GetNewPath(path);
+            File.WriteAllBytes(newPath, originalBytes);
+            Console.WriteLine($"Patched file saved as {newPath}");
         }
 
         PressAnyKeyToExit();
     }
 
-    private static bool FindAndPatch(string filePath)
+    private static string GetNewPath(string path)
     {
-        byte[] searchPattern =
-        [
-            0x75, 0x37, 0x8B, 0x4D, 0x0C, 0x85, 0xC9, 0x74, 
-            0x1C, 0x0F, 0xB7, 0x01, 0x66, 0x85, 0xC0, 0x74
-        ];
-            
-        byte[] replacePattern = [0xEB, 0x37];
+        var count = 1;
+        var directory = Path.GetDirectoryName(path)!;
+        var newPath = Path.Combine(directory, $"{PatchedName}.exe");
         
-        var fileBytes = File.ReadAllBytes(filePath);
+        while (File.Exists(newPath))
+        {
+            count++;
+            newPath = Path.Combine(directory, $"{PatchedName}_{count}.exe");
+        }
+
+        return newPath;
+    }
+
+    private static bool TryPatch(ref byte[] bytes, Patch patch)
+    {
+        try
+        {
+            if (IsPatched(bytes, patch))
+            {
+                Console.WriteLine($"[{patch.Name}] Already patched");
+                return false;
+            }
+            
+            var result = FindAndPatch(ref bytes, patch);
+            if (!result)
+            {
+                Console.WriteLine($"[{patch.Name}] Target pattern not found in the file");
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return false;
+        }
+    }
+
+    private static bool FindAndPatch(ref byte[] bytes, Patch patch)
+    {
         var patternFound = false;
 
-        for (var i = 0; i < fileBytes.Length - searchPattern.Length; i++)
+        for (var i = 0; i < bytes.Length - patch.Pattern.Length; i++)
         {
-            if (fileBytes.Skip(i).Take(searchPattern.Length).SequenceEqual(searchPattern))
+            if (bytes.Skip(i).Take(patch.Pattern.Length).SequenceEqual(patch.Pattern))
             {
-                Console.WriteLine($"Pattern founded: 0x{i:X8}");
+                Console.WriteLine($"[{patch.Name}] Pattern founded: 0x{i:X8}");
                 
                 Buffer.BlockCopy(
-                    replacePattern, 
-                    0, 
-                    fileBytes, 
-                    i,
-                    replacePattern.Length
+                    patch.Replace, 
+                    0,
+                    bytes, 
+                    i + patch.Offset,
+                    patch.Replace.Length
                 );
 
                 patternFound = true;
@@ -78,30 +116,21 @@ class Program
 
         if (patternFound)
         {
-            File.Copy(filePath, PatchedName, true);
-            var newPath = Path.Combine(Path.GetDirectoryName(filePath)!, PatchedName);
-            File.WriteAllBytes(newPath, fileBytes);
-            Console.WriteLine($"Patched file created at: {newPath}");
+            Console.WriteLine($"[{patch.Name}] Patched");
         }
 
         return patternFound;
     }
 
-    private static bool IsPatched(string filePath)
+    private static bool IsPatched(byte[] bytes, Patch patch)
     {
         try
         {
-            byte[] patchedPattern =
-            [
-                0xEB, 0x37, 0x8B, 0x4D, 0x0C, 0x85, 0xC9, 0x74, 
-                0x1C, 0x0F, 0xB7, 0x01, 0x66, 0x85, 0xC0, 0x74
-            ];
+            var patchedPattern = patch.GetExpectedResult();
 
-            var fileBytes = File.ReadAllBytes(filePath);
-            
-            for (var i = 0; i < fileBytes.Length - patchedPattern.Length; i++)
+            for (var i = 0; i < bytes.Length - patchedPattern.Length; i++)
             {
-                if (fileBytes.Skip(i).Take(patchedPattern.Length).SequenceEqual(patchedPattern))
+                if (bytes.Skip(i).Take(patchedPattern.Length).SequenceEqual(patchedPattern))
                 {
                     return true;
                 }
